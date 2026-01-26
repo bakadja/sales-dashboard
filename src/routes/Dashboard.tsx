ae4305c5bc3eb6ad86a3b1c0eafb4937715b454c
ngongang <contact@kevinpaulidor.de>
2026-01-26 20:25:55 +0100

    Add Dashboard component with metrics visualization and real-time updates

---
+++ src/routes/Dashboard.tsx
@@ -0,0 +1,104 @@
+import supabase from '../supabase-client';
+import { useEffect, useState } from 'react';
+import {
+  Bar,
+  BarChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import Form from '../components/Form';
+import type { Metric } from '../types/metrics';
+
+function Dashboard() {
+  const [metrics, setMetrics] = useState<Metric[]>([]);
+
+  useEffect(() => {
+    fetchMetrics();
+
+
+    const channel = supabase
+      .channel('deal-changes')
+      .on(
+        'postgres_changes',
+        { 
+          event: '*', 
+          schema: 'public', 
+          table: 'sales_deals' 
+        },
+        payload => {
+          console.log(payload);
+          fetchMetrics();
+        })
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
+
+  async function fetchMetrics() {
+    try {
+      const { data, error } = await supabase.from('sales_deals').select(
+        `
+          name,
+          value.sum()
+          `
+      );
+      if (error) {
+        throw error;
+      }
+      setMetrics((data ?? []) as Metric[]);
+    } catch (error) {
+      console.error('Error fetching metrics:', error);
+    }
+  }
+
+  const chartData = [
+    {
+      data: metrics.map((m) => ({
+        name: m.name,
+        value: m.sum ?? 0,
+      })),
+    },
+  ];
+
+  function y_max() {
+    if (metrics.length > 0) {
+      const maxSum = Math.max(...metrics.map((m) => m.sum ?? 0));
+      return maxSum + 2000;
+    };
+    return 5000;
+  };
+
+  return (
+    <div
+      className="dashboard-wrapper"
+      role="region"
+      aria-label="Sales dashboard"
+    >
+      <div
+        className="chart-container"
+        role="region"
+        aria-label="Sales chart and data"
+      >
+        <h2>Total Sales This Quarter ($)</h2>
+        <div style={{ flex: 1 }}>
+          <ResponsiveContainer width="100%" height={320}>
+            <BarChart data={chartData[0].data}>
+              <XAxis dataKey="name" />
+              <YAxis domain={[0, y_max()]} />
+              <Tooltip />
+              <Bar dataKey="value" fill="#58d675" />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+      <Form metrics={metrics} />
+    </div>
+  );
+};
+
+export default Dashboard;
