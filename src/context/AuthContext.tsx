21a765d338a308b540111a118e152f9e7d2634fe
ngongang <contact@kevinpaulidor.de>
2026-01-26 20:25:38 +0100

    Add AuthContext, AuthContextProvider, and useAuth hook for authentication management

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, type ReactNode } from 'react';
+import type { Session } from '@supabase/supabase-js';
+import supabase from '../supabase-client';
+import { AuthContext } from './auth-context';
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContextProvider = ({ children }: AuthContextProviderProps) => {
+//Session state (user info, sign-in status)
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+
+  (async function getInitialSession(){
+    try {
+       const { data, error } =  await supabase.auth.getSession()
+      
+      if(error) {
+       throw error
+      }
+
+      console.log("data.session",data.session)
+      setSession(data.session)
+    } catch(err) {
+      console.error("failed to fecth session", err)
+    }
+  })()
+
+  //2) Listen for changes in auth state
+
+  }, []);
+
+//Auth functions (signin, signup, logout)
+
+
+  return (
+    <AuthContext.Provider value={{ session }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
